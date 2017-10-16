52cc14e10830e4dc7f869dfd475a00cedc54faab
Thallison Rhanniel <thallisonrlima@gmail.com>
2017-10-16 16:00:30 -0300

    Add requirements.txt file

--- projectDjangoTest/settings.py
+++ projectDjangoTest/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+from decouple import config # python-decouple <-- Lib
+import dj_database_url # dj-database-url <-- Lib
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -147,8 +148,14 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #-----------------------------
 
-
-
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 
 
